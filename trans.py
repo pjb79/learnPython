1d15a25f21a36c567aa2984417d6edc352802909
Peter <pjb79@hotmail.com>
2017-02-19 22:32:17 +1100

    1

--- trans.py
+++ trans.py
@@ -6,10 +6,17 @@ Created on Mon Feb  6 16:48:09 2017
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime as dt
+import pandas as pd
+
+
+plt.close('all')
 
 # import data to pandas
-import pandas as pd
-df              = pd.read_csv('C:\\Users\\PBallant\\Downloads\\Transactions (4).csv')
+df              = pd.read_csv('C:\\Users\\pjbca\\Downloads\\Transactions.csv')
+
+# drop unwanted columns
+df.drop(['Credit', 'Balance'], axis = 1, inplace = True)
 
 # get transaction date from description column
 df['transDate'] = df['Description'].str.extract('Date.(\w\w.\w\w\w.\w\w\w\w)', expand = False)
@@ -25,36 +32,60 @@ ind             = pd.notnull(df['Debit'])
 df              = df[ind]
 
 # add week number to df
-df['weekNum']      = df['transDate'].dt.week.astype('str')
-#df['weekNumStr']   = df['weekNum']
-df['yearNum']      = df['transDate'].dt.year.astype('str')
-#df['yearNumStr']   = df['yearNum'].astype('str')
-df['weekIdent']    = df['weekNum'] + df['yearNum']
-df['Qdate']        = [date - pd.tseries.offsets.DateOffset(days=1) + pd.tseries.offsets.Week() for date in  df.Date]
-df['dayNum']       = df['transDate'].dt.weekday_name
+df['dayNum']    = df['transDate'].dt.weekday
+df['weekBegin'] = df.apply(lambda x: x['transDate'] - dt.timedelta(days=x['dayNum']), axis=1)
+df['dayName']   = df['transDate'].dt.weekday_name
+df['monthEnd']  = df['transDate'] + pd.tseries.offsets.MonthEnd(1)
 
 # get unique week identifier
-uniqueWeeks = df['weekIdent'].unique()
+uniqueWeeks     = df['weekBegin'].unique()
 
 # get rid of weeks that don't have a full seven days (ie last week of year)
 for w in uniqueWeeks:
-    if len(df[df['weekIdent'] == w])<7:
-        df = df[df['weekIdent'] != w]
+    if len(df[df['weekBegin'] == w])<7:
+        df = df[df['weekBegin'] != w]
         
 
 # pivot
-table = df.pivot_table(index = 'weekIdent', columns = 'dayNum', values = 'Debit', fill_value = 0, aggfunc = np.sum)
-table['sum'] = table.sum(axis = 1)
-table = table.div(table['sum'], axis = 0)
-table = table.drop('sum', axis = 1)
-table = table[['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']]
+daysOfWeek      = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
 
+table           = df.pivot_table(index = 'weekBegin', columns = 'dayName', values = 'Debit', fill_value = 0, aggfunc = np.sum)
+table['sum']    = table.sum(axis = 1)
+table           = table.div(table['sum'], axis = 0)
+table           = table.drop('sum', axis = 1)
+table           = table[daysOfWeek]
+
+
+# create plots
 plt.figure()
 table.boxplot()
+plt.ylim([0,1])
+plt.ylabel('Percentage of Weekly Spend')
 
 plt.figure()
-plt.plot(table['Monday'])
+for d in daysOfWeek:
+    plt.subplot(3,3,daysOfWeek.index(d)+1)
+    plt.plot(table[d])
+    plt.ylim([0,1])
+    plt.ylabel('Percentage of Weekly Spend')
+    plt.title(d)
+    
+# get cash
+df['cash']  = df['Description'].str.extract('Cash amount \$(\d*\.\d*)<BR', expand = False).fillna(0).astype('float')
+df['cash1'] = df.loc[df['Description'].str.contains('ATM owner fee'),['Debit']]
+df['cash1'] = df['cash1'].fillna(0)
+df['cash']  = -1*df['cash'] + df['cash1']
+df          = df.drop('cash1', axis = 1)
 
+dfCash              = df.groupby(by = 'monthEnd').sum()
+dfCash['pctCash']   = dfCash['cash'] / dfCash['Debit']
+plt.figure()
+plt.plot(dfCash['pctCash'])
+
+
+# break out sales description
+df['vendor']  = df['Description'].str.extract('(.*).-.Receipt', expand = False)
+df['vendor']  = df['vendor'].str.extract('(.*).-', expand = False)
 
 
 
