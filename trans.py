e39e006607ddee82ee515f3dc868c2d8dfc71009
Peter Ballantyne <pjb79@hotmail.com>
2017-02-20 16:57:41 +1100

    1

--- trans.py
+++ trans.py
@@ -12,11 +12,13 @@ import pandas as pd
 
 plt.close('all')
 
-# import data to pandas
-df              = pd.read_csv('C:\\Users\\pjbca\\Downloads\\Transactions.csv')
+## import data to pandas and prepare 
+#df              = pd.read_csv('C:\\Users\\pjbca\\Downloads\\Transactions.csv')
+df              = pd.read_csv('C:\\Users\\PBallant\\Downloads\\Transactions (4).csv')
 
 # drop unwanted columns
 df.drop(['Credit', 'Balance'], axis = 1, inplace = True)
+
 
 # get transaction date from description column
 df['transDate'] = df['Description'].str.extract('Date.(\w\w.\w\w\w.\w\w\w\w)', expand = False)
@@ -25,11 +27,8 @@ df['transDate'] = df['Description'].str.extract('Date.(\w\w.\w\w\w.\w\w\w\w)', e
 df['Date']      = pd.to_datetime(df['Date'], format = '%d/%m/%Y')
 df['transDate'] = pd.to_datetime(df['transDate'], format = '%d %b %Y', exact = False)
 
-# remove entries with no datetme and no debit amount
-ind             = pd.notnull(df['transDate'])
-df              = df[ind]
-ind             = pd.notnull(df['Debit'])
-df              = df[ind]
+# for entries with no trans date uses ordinary date
+df['transDate'].fillna(df['Date'], inplace=True)
 
 # add week number to df
 df['dayNum']    = df['transDate'].dt.weekday
@@ -37,26 +36,44 @@ df['weekBegin'] = df.apply(lambda x: x['transDate'] - dt.timedelta(days=x['dayNu
 df['dayName']   = df['transDate'].dt.weekday_name
 df['monthEnd']  = df['transDate'] + pd.tseries.offsets.MonthEnd(1)
 
+# remove entries with no debit amount
+ind             = pd.notnull(df['Debit'])
+df              = df[ind]
+
+# adjust entries where cash is taken
+df['cash']  = df['Description'].str.extract('Cash amount \$(\d*\.\d*)<BR', expand = False).fillna(0).astype('float')
+ind = df['cash'] > 0
+dfCash = df.loc[ind,:]
+dfCash.loc[:,['Description']] = 'Cash'
+dfCash.loc[:,'Debit'] = -dfCash.loc[:,'cash']
+df.loc[ind,'Debit'] = df.loc[ind, 'Debit'] + df.loc[ind, 'cash']
+df = df.append(dfCash)
+df = df.drop('cash', axis = 1)
+
+df.loc[df['Description'].str.contains('ATM owner fee'),['Description']] = 'Cash'
+
+# copy dataframe for use in day of week calcs
+dfWeekDay = df.copy()
+
 # get unique week identifier
-uniqueWeeks     = df['weekBegin'].unique()
+uniqueWeeks     = dfWeekDay['weekBegin'].unique()
 
 # get rid of weeks that don't have a full seven days (ie last week of year)
 for w in uniqueWeeks:
-    if len(df[df['weekBegin'] == w])<7:
-        df = df[df['weekBegin'] != w]
+    if len(dfWeekDay[dfWeekDay['weekBegin'] == w])<7:
+        dfWeekDay = dfWeekDay[dfWeekDay['weekBegin'] != w]
         
 
 # pivot
 daysOfWeek      = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
 
-table           = df.pivot_table(index = 'weekBegin', columns = 'dayName', values = 'Debit', fill_value = 0, aggfunc = np.sum)
+table           = dfWeekDay.pivot_table(index = 'weekBegin', columns = 'dayName', values = 'Debit', fill_value = 0, aggfunc = np.sum)
 table['sum']    = table.sum(axis = 1)
 table           = table.div(table['sum'], axis = 0)
 table           = table.drop('sum', axis = 1)
 table           = table[daysOfWeek]
 
-
-# create plots
+# create plots of percentage of spending by day of week
 plt.figure()
 table.boxplot()
 plt.ylim([0,1])
@@ -70,13 +87,8 @@ for d in daysOfWeek:
     plt.ylabel('Percentage of Weekly Spend')
     plt.title(d)
     
-# get cash
-df['cash']  = df['Description'].str.extract('Cash amount \$(\d*\.\d*)<BR', expand = False).fillna(0).astype('float')
-df['cash1'] = df.loc[df['Description'].str.contains('ATM owner fee'),['Debit']]
-df['cash1'] = df['cash1'].fillna(0)
-df['cash']  = -1*df['cash'] + df['cash1']
-df          = df.drop('cash1', axis = 1)
 
+# calc proportion of cash used
 dfCash              = df.groupby(by = 'monthEnd').sum()
 dfCash['pctCash']   = dfCash['cash'] / dfCash['Debit']
 plt.figure()
