ffb7f67697a0b8cc7394fa883b2a11190b9e17b0
Peter Ballantyne <pjb79@hotmail.com>
2017-05-16 12:10:32 +1000

    1

--- DAT210x-master/Module4/assignment5.py
+++ DAT210x-master/Module4/assignment5.py
@@ -4,17 +4,22 @@ from scipy import misc
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib
 import matplotlib.pyplot as plt
+from os import listdir
+from os.path import isfile, join
+from sklearn import manifold
+
 
 # Look pretty...
 # matplotlib.style.use('ggplot')
 plt.style.use('ggplot')
-
+plt.close('all')
 
 #
 # TODO: Start by creating a regular old, plain, "vanilla"
 # python list. You can call it 'samples'.
 #
-samples = []
+dset = []
+colours = []
 
 #
 # TODO: Write a for-loop that iterates over the images in the
@@ -22,13 +27,25 @@ samples = []
 # your list. Each .PNG image should first be loaded into a
 # temporary NDArray, just as shown in the Feature
 # Representation reading.
+
+filePath = 'C:\\D\\GitHub\\learnPython\\DAT210x-master\\Module4\\Datasets\\ALOI\\32\\'
+allFiles = [f for f in listdir(filePath) if isfile(join(filePath, f))]
+
+
+
+for f in allFiles:
+    tempArray = misc.imread(filePath + f)
+    dset.append(tempArray.reshape(-1))
+    colours.append('b')
+    
+    
 #
 # Optional: Resample the image down by a factor of two if you
 # have a slower computer. You can also convert the image from
 # 0-255  to  0.0-1.0  if you'd like, but that will have no
 # effect on the algorithm's results.
 #
-y = misc.imread()
+
 
 
 #
@@ -38,13 +55,18 @@ y = misc.imread()
 # in the Module4/Datasets/ALOI/32_i directory. Re-run your
 # assignment and answer the final question below.
 #
-# .. your code here .. 
+filePath = 'C:\\D\\GitHub\\learnPython\\DAT210x-master\\Module4\\Datasets\\ALOI\\32i\\'
+allFiles = [f for f in listdir(filePath) if isfile(join(filePath, f))]
 
+for f in allFiles:
+    tempArray = misc.imread(filePath + f)
+    dset.append(tempArray.reshape(-1))
+    colours.append('r')
 
 #
 # TODO: Convert the list to a dataframe
 #
-# .. your code here .. 
+dset = pd.DataFrame(dset)
 
 
 
@@ -52,7 +74,9 @@ y = misc.imread()
 # TODO: Implement Isomap here. Reduce the dataframe df down
 # to three components, using K=6 for your neighborhood size
 #
-# .. your code here .. 
+iso = manifold.Isomap(n_neighbors=6, n_components=3)
+iso.fit(dset)
+transformT = iso.transform(dset)
 
 
 
@@ -61,7 +85,10 @@ y = misc.imread()
 # can use either 'o' or '.' as your marker. Graph the first two
 # isomap components
 #
-# .. your code here .. 
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_title('2D Scatter')
+ax.scatter(transformT[:,0],transformT[:,2], marker='.',alpha=0.7, c = colours)
 
 
 
@@ -70,9 +97,15 @@ y = misc.imread()
 # TODO: Create a 3D Scatter plot to graph your manifold. You
 # can use either 'o' or '.' as your marker:
 #
-# .. your code here .. 
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_title('3D Scatter')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.scatter(transformT[:,0], transformT[:,1], transformT[:,2], c=colours, marker='.', alpha=0.75)
 
 
 
-plt.show()
+#plt.show()
 
