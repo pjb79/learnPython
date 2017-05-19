52253fc23ec21a356a2b815375be6b047a530fa3
Peter Ballantyne <pjb79@hotmail.com>
2017-05-19 15:33:54 +1000

    1

--- DAT210x-master/Module5/assignment1.py
+++ DAT210x-master/Module5/assignment1.py
@@ -1,13 +1,15 @@
 #
 # TODO: Import whatever needs to be imported to make this work
 #
-# .. your code here ..
-
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
 # Look Pretty
 # matplotlib.style.use('ggplot')
 #plt.style.use('ggplot')
-
+plt.close('all')
 
 #
 # TODO: To procure the dataset, follow these steps:
@@ -32,45 +34,47 @@ def doKMeans(df):
   # TODO: Filter df so that you're only looking at Longitude and Latitude,
   # since the remaining columns aren't really applicable for this purpose.
   #
-  # .. your code here ..
+  df = df.loc[:,['Longitude','Latitude']]
 
   #
   # TODO: Use K-Means to try and find seven cluster centers in this df.
   # Be sure to name your kmeans model `model` so that the printing works.
   #
-  # .. your code here ..
+  model = KMeans(n_clusters = 7)
+  model.fit(df)
 
   #
   # INFO: Print and plot the centroids...
   centroids = model.cluster_centers_
   ax.scatter(centroids[:,0], centroids[:,1], marker='x', c='red', alpha=0.5, linewidths=3, s=169)
-  print centroids
+  print(centroids)
 
 
 
 #
 # TODO: Load your dataset after importing Pandas
 #
-# .. your code here ..
+df = pd.read_csv('C:\\D\GitHub\\learnPython\\DAT210x-master\\Module5\\Datasets\\Crimes_-_2001_to_present.csv', index_col = 'ID')
 
 
 #
 # TODO: Drop any ROWs with nans in them
 #
-# .. your code here ..
+df = df.dropna(axis = 0, how = 'any')
 
 
 #
 # TODO: Print out the dtypes of your dset
 #
-# .. your code here ..
+print(df.dtypes)
 
 
 #
 # Coerce the 'Date' feature (which is currently a string object) into real date,
 # and confirm by re-printing the dtypes. NOTE: This is a slow process...
 #
-# .. your code here ..
+df['Date'] = pd.to_datetime(df['Date'])
+print(df.dtypes)
 
 
 # INFO: Print & Plot your data
@@ -82,7 +86,7 @@ doKMeans(df)
 # a Date > '2011-01-01', using indexing. Then, in a new figure, plot the
 # crime incidents, as well as a new K-Means run's centroids.
 #
-# .. your code here ..
+df = df.loc[df['Date']>'2011-01-01',:]
 
 
 
